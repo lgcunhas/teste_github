b87e334037ce023d1b3d24d6c73c22d6712235dc
lgcunhas <lgcunhas@gmail.com>
2021-01-06 11:17:23 -0300

    Criacao do main e redme

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  6 10:35:17 2021
+
+@author: lgcunhas
+"""
+
+import pandas as pd
+import seaborn as sns
+import numpy as np
+
